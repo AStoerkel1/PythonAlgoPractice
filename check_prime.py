b33307cfa3a203f4ba1be3a9f4a971e7f2769cea
AStoerkel1 <41537999+AStoerkel1@users.noreply.github.com>
2018-07-22 06:44:42 -0400

    Create check_prime.py

---
+++ check_prime.py
@@ -0,0 +1,24 @@
+
+
+#check_prime
+
+from math import sqrt
+nums = []
+def divisors(n):
+    for i in range(1, int(sqrt(n) + 1)):
+        if n % i == 0:
+            nums.append(i)
+            if i * i != n:
+                nums.append(n // i)
+    print(nums)
+    return nums
+
+
+def check_p(n):
+    a = len(divisors(n))
+    if a == 2:
+        return True
+    else:
+        return False
+
+#print(check_p(91))
