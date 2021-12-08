3ea08e11d59a3f7ee1e2e0a5a69a39bc7d0c0b6f
Andrew <astoer1@students.towson.edu>
2021-12-08 13:33:32 -0500

    made transpose more understandable

--- matrixOps.py
+++ matrixOps.py
@@ -11,10 +11,10 @@ class matrixOps:
 
 #transpose the matrix		
 	def transpose(self, A):
-		return [[A[j][i] for j in range(len(A))] for i in range(len(A[0]))]
+		return [list(x) for x in zip(*A)]
 
 #multiply matricies (ngl i have no idea how this works i found it online)
-	def transMult(self, A, B):
+	def multMat(self, A, B):
 		return [[sum(a * b for a, b in zip(A_row, B_col))
 				for B_col in zip(*B)]
 					for A_row in A]	
@@ -22,7 +22,7 @@ class matrixOps:
 #solve a system of equations (unfortunately the matrix has to be square
 	def leastSquares(self):
 		Atrans = self.transpose(self.A)
-		ATA = self.transMult(Atrans, A)
+		ATA = self.multMat(Atrans, A)
 		return np.linalg.inv(ATA).dot(self.y)
 
 A = np.array([[1, 2],[3, 4],[5,6]])
@@ -32,4 +32,5 @@ y = np.array([1,3])
 multiple = matrixOps(A, y)
 
 print(multiple.leastSquares())
+
 
--- teste.py
+++ teste.py
@@ -12,10 +12,12 @@ into
 notation: A[row][col]
 	  A[outer][inner]
 '''
-ls = [[1,2], [3,4], [5,6]]
+A = [[1,2], [3,4], [5,6]]
+B = [[1, 2],[3,4]]
 
+newLs = [[A[i][j] for i in range(len(A))]for j in range(len(A[0]))]
 
-newLs = [[ls[i][j] for i in range(len(ls))]for j in range(len(ls[0]))]
+for A_row in A:
+	for B_col in zip(*B):
+		print([sum(a * b for a, b in zip(A_row, B_col))])
 
-print(newLs)
-print(ls[0][1])
