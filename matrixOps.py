8a169d7903a8a8c4ff1ef96557d9a2b674f64e0d
Andrew <astoer1@students.towson.edu>
2021-12-08 15:33:09 -0500

    fixed multipliy so it can take vectors

--- matrixOps.py
+++ matrixOps.py
@@ -1,10 +1,11 @@
 ''' 
 this is supposed to perform a bunch of operations for matricies
+
 '''
 
 import numpy as np
 class matrixOps:
-#A = any matrix, y = a multiplicable vector
+#A = any matrix, y = a multiplicable vector or matrix
 	def __init__(self, A, y):
 		self.A = A
 		self.y = y
@@ -13,13 +14,16 @@ class matrixOps:
 	def transpose(self, A):
 		return [list(x) for x in zip(*A)]
 
-#multiply matricies (ngl i have no idea how this works i found it online)
+#multiply matricies (i found the mat*mat alg online but modified it for vectors)
 	def multMat(self, A, B):
-		return [[sum(a * b for a, b in zip(A_row, B_col))
-				for B_col in zip(*B)]
-					for A_row in A]	
+#B is a matrix
+		if type(B[0]) is (np.ndarray):
+			return [[sum(a * b for a, b in zip(A_row, B_col))for B_col in zip(*B)]for A_row in A]
+#B is a vector
+		else:
+			return [sum(a * b for a, b in zip(A_row, B)) for A_row in A]
 
-#solve a system of equations (unfortunately the matrix has to be square
+#solve a system of equations (unfortunately the matrix has to be square)
 	def leastSquares(self):
 		Atrans = self.transpose(self.A)
 		ATA = self.multMat(Atrans, A)
@@ -27,10 +31,9 @@ class matrixOps:
 
 A = np.array([[1, 2],[3, 4],[5,6]])
 
-y = np.array([1,3])
+y = np.array([[1,2], [3,4]])
 
 multiple = matrixOps(A, y)
 
-print(multiple.leastSquares())
-
-
+print(multiple.multMat(A, y))
+print(multiple.multMat(A, y[0]))
