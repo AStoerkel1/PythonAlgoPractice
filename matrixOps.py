ad0fb9cb88e36bbd5171d5845c35f809e06a44f1
Andrew <astoer1@students.towson.edu>
2021-12-08 15:52:26 -0500

    i forgor

--- matrixOps.py
+++ matrixOps.py
@@ -1,39 +1,39 @@
 ''' 
 this is supposed to perform a bunch of operations for matricies
-
+I'm too lazy to add any type checking rn
 '''
 
 import numpy as np
 class matrixOps:
-#A = any matrix, y = a multiplicable vector or matrix
-	def __init__(self, A, y):
+#A = any matrix, B = a multiplicable vector or matrix
+	def __init__(self, A, B):
 		self.A = A
-		self.y = y
+		self.B = B
 
 #transpose the matrix		
-	def transpose(self, A):
-		return [list(x) for x in zip(*A)]
+	def transpose(self):
+		return [list(x) for x in zip(*self.A)]
 
 #multiply matricies (i found the mat*mat alg online but modified it for vectors)
-	def multMat(self, A, B):
+	def multMat(self):
 #B is a matrix
-		if type(B[0]) is (np.ndarray):
-			return [[sum(a * b for a, b in zip(A_row, B_col))for B_col in zip(*B)]for A_row in A]
+		if type(self.B[0]) is (np.ndarray):
+			return [[sum(a * b for a, b in zip(A_row, B_col))for B_col in zip(*self.B)]for A_row in self.A]
 #B is a vector
 		else:
-			return [sum(a * b for a, b in zip(A_row, B)) for A_row in A]
+			return [sum(a * b for a, b in zip(A_row, self.B)) for A_row in self.A]
 
-#solve a system of equations (unfortunately the matrix has to be square)
+#given a matrix and vector that are inconsistant, this will find the least squares solution
 	def leastSquares(self):
 		Atrans = self.transpose(self.A)
 		ATA = self.multMat(Atrans, A)
 		return np.linalg.inv(ATA).dot(self.y)
 
 A = np.array([[1, 2],[3, 4],[5,6]])
+y = np.array([1,2])
+B = np.array([[1,2], [3,4]])
 
-y = np.array([[1,2], [3,4]])
-
-multiple = matrixOps(A, y)
-
-print(multiple.multMat(A, y))
-print(multiple.multMat(A, y[0]))
+BMat = matrixOps(A, B)
+yMat = matrixOps(A, y)
+print(BMat.multMat())
+print(yMat.multMat())
