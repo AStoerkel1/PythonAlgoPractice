1fe867547421a7039ae67dd3edc54381b746e911
Andrew <astoer1@students.towson.edu>
2021-12-08 11:30:48 -0500

    made it more general

--- matrixOps.py
+++ matrixOps.py
@@ -8,31 +8,28 @@ class matrixOps:
 	def __init__(self, A, y):
 		self.A = A
 		self.y = y
+
 #transpose the matrix		
 	def transpose(self, A):
 		return [[A[j][i] for j in range(len(A))] for i in range(len(A[0]))]
-#multiply matrix by it's transpose
-	def transMult(self, AT, A):
-		return [[sum(a * b for a, b in zip(AT_row, A_col))
-				for A_col in zip(*A)]
-					for AT_row in AT]	
 
+#multiply matricies (ngl i have no idea how this works i found it online)
+	def transMult(self, A, B):
+		return [[sum(a * b for a, b in zip(A_row, B_col))
+				for B_col in zip(*B)]
+					for A_row in A]	
 
-A = np.array([[1, 2],[3, 4],[5, 6]])
+#solve a system of equations (unfortunately the matrix has to be square
+	def leastSquares(self):
+		Atrans = self.transpose(self.A)
+		ATA = self.transMult(Atrans, A)
+		return np.linalg.inv(ATA).dot(self.y)
+
+A = np.array([[1, 2],[3, 4],[5,6]])
 
 y = np.array([1,3])
 
 multiple = matrixOps(A, y)
 
-Atrans = multiple.transpose(A)
-
-for r in Atrans:
-	print(r)
-
-ans = multiple.transMult(Atrans, A)
-
-for r in ans:
-	print(r)
-
-
+print(multiple.leastSquares())
 
