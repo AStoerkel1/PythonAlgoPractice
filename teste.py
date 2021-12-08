9d8f2ae6e512553269cc1dfa0f74d69a02e4ffa3
Andrew <astoer1@students.towson.edu>
2021-12-08 10:20:21 -0500

    testing

--- teste.py
+++ teste.py
@@ -1,7 +1,21 @@
-def front_back(string):
-  start = string[0]
-  end = string[len(string)-1]
-  s1 = string.lstrip(start)
-  s2 = s1.rstrip(end)
-  return end + s2 + start
-print(front_back('supercalifragilisticexpialidotion'))
+#test some compositional statements
+''' 
+1 2
+3 4
+5 6
+
+into 
+
+1 3 5
+2 4 6
+
+notation: A[row][col]
+	  A[outer][inner]
+'''
+ls = [[1,2], [3,4], [5,6]]
+
+
+newLs = [[ls[i][j] for i in range(len(ls))]for j in range(len(ls[0]))]
+
+print(newLs)
+print(ls[0][1])
