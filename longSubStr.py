63fc51fbec20371abf5dc225add08e5f9a640c23
Andrew <astoer1@students.towson.edu>
2021-12-27 11:53:09 -0500

    finished

--- longSubStr.py
+++ longSubStr.py
@@ -1,26 +1,21 @@
 def longestSubString(s: str):
     subStr = ""
     temp = ""
-    
-    for i in s:
-        if temp + i in s and i not in temp:
+    for  i in s:
+        if i not in temp:
             temp = temp+i
         else:
-            temp = i
+            temp = temp[temp.find(i)+1:]+ i
+            #print(temp)
         if len(temp) > len(subStr):
             subStr = temp
-        
+            print(subStr)
     return len(subStr)
-
-
-'''
-i think this need to be solved with a nested loop but I'm not sure right now
-'''
 
 '''
 s="abcabcbb"
 s="pwwkew"
 s="bbbb"
 '''
-s = "dvdf"
+s = "aabaab!bb"
 print(longestSubString(s))
