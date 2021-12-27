12e807d74a6f65790055bd5deaef1cc4b74f34ca
Andrew <astoer1@students.towson.edu>
2021-12-27 11:57:45 -0500

    add comments

--- longSubStr.py
+++ longSubStr.py
@@ -1,15 +1,17 @@
 def longestSubString(s: str):
     subStr = ""
     temp = ""
+    #loop through the string
     for  i in s:
+        #if the char isn't already in the temp string then append
         if i not in temp:
             temp = temp+i
         else:
+            #if it is, then find its first occurance in temp
+            #and start the new temp from +1 index higher
             temp = temp[temp.find(i)+1:]+ i
-            #print(temp)
         if len(temp) > len(subStr):
             subStr = temp
-            print(subStr)
     return len(subStr)
 
 '''
