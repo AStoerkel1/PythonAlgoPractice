7a931d8073e89de121cfc5dcf8508ad113dc9799
Andrew <astoer1@students.towson.edu>
2022-02-06 14:56:32 -0500

    got it fully working now

--- isBad.py
+++ isBad.py
@@ -10,20 +10,16 @@ def firstBadVersion(n: int) -> int:
     
     min = 1
     max = n
-    mid = n//2
     while min <= max:
         mid = (min + max) // 2
         if min == max:
-            if not isBadVersion(mid):
-                return mid + 1
-            else:
-                return mid 
+            return mid 
         if isBadVersion(mid):
             print("-1")
-            max = mid - 1
+            max = mid
         else:
             print("+1")
             min = mid + 1
 
 
-print(firstBadVersion(0))
+print(firstBadVersion(12))
